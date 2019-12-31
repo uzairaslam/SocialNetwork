e38e2dd467852a310de7cdff307a1c780785136c
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2019-12-31 23:40:40 +0500

    Security initial commit

---
+++ SocialNetwork.API/Data/Concrete/AuthRepository.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using SocialNetwork.API.Data.Interfaces;
+using SocialNetwork.API.Models;
+
+namespace SocialNetwork.API.Data.Concrete
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
