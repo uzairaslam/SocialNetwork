d66599d313c81077fc9a98e053eafb149a2e60a6
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2020-01-08 13:15:29 +0530

    Login Repository Method

--- SocialNetwork.API/Startup.cs
+++ SocialNetwork.API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SocialNetwork.API.Data;
+using SocialNetwork.API.Data.Concrete;
+using SocialNetwork.API.Data.Interfaces;
 
 namespace SocialNetwork.API
 {
@@ -30,6 +32,7 @@ namespace SocialNetwork.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository,AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
