a9b437acb50b0cbc5cf79df8d0c42e118827d6c4
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2020-02-09 21:15:13 +0500

    nav dropdown for user

--- SocialNetwork-SPA/src/app/nav/nav.component.ts
+++ SocialNetwork-SPA/src/app/nav/nav.component.ts
@@ -21,4 +21,14 @@ export class NavComponent implements OnInit {
       console.log('Error occured');
     });
   }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged Out');
+  }
 }
